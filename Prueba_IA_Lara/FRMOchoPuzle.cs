d48e20cac9708f6e53282b3c2c6bfec37dffa1b8
Laboratorio1 <Laboratorio1@DESKTOP-PFN2SR9>
2026-03-30 08:39:56 -0400

    Agregar archivos de proyecto.

---
+++ Prueba_IA_Lara/FRMOchoPuzle.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_IA_Lara
+{
+    public partial class FRMOchoPuzle : Form
+    {
+
+        private int contador;
+        public FRMOchoPuzle()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LBL21_Click(object sender, EventArgs e)
+        {
+            if (LBL22.Text == "0")
+            {
+                LBL22.Text = LBL21.Text;
+                LBL21.Text = "0";
+            }
+
+            if (LBL20.Text == "0")
+            {
+                LBL20.Text = LBL21.Text;
+                LBL21.Text = "0";
+            }
+
+            if (LBL11.Text == "0")
+            {
+                LBL11.Text = LBL21.Text;
+                LBL21.Text = "0";
+            }
+        }
+
+        private void LBL11_Click(object sender, EventArgs e)
+        {
+            if (LBL21.Text == "0")
+            {
+                LBL21.Text = LBL11.Text;
+                LBL11.Text = "0";
+            }
+
+            if (LBL10.Text == "0")
+            {
+                LBL10.Text = LBL11.Text;
+                LBL11.Text = "0";
+            }
+
+            if (LBL12.Text == "0")
+            {
+                LBL12.Text = LBL11.Text;
+                LBL11.Text = "0";
+            }
+            if (LBL01.Text == "0")
+            {
+                LBL01.Text = LBL11.Text;
+                LBL11.Text = "0";
+            }
+        }
+
+        private void LBL20_Click(object sender, EventArgs e)
+        {
+            if (LBL21.Text == "0")
+            {
+                LBL21.Text = LBL20.Text;
+                LBL20.Text = "0";
+            }
+
+            if (LBL10.Text == "0")
+            {
+                LBL10.Text = LBL20.Text;
+                LBL20.Text = "0";
+            }
+        }
+
+        private void LBL22_Click(object sender, EventArgs e)
+        {
+            if (LBL21.Text == "0")
+            {
+                LBL21.Text = LBL22.Text;
+                LBL22.Text = "0";
+            }
+
+            if (LBL12.Text == "0")
+            {
+                LBL12.Text = LBL22.Text;
+                LBL22.Text = "0";
+            }
+        }
+
+        private void LBL12_Click(object sender, EventArgs e)
+        {
+            if (LBL22.Text == "0")
+            {
+                LBL22.Text = LBL12.Text;
+                LBL12.Text = "0";
+            }
+
+            if (LBL11.Text == "0")
+            {
+                LBL11.Text = LBL12.Text;
+                LBL12.Text = "0";
+            }
+
+            if (LBL02.Text == "0")
+            {
+                LBL02.Text = LBL12.Text;
+                LBL12.Text = "0";
+            }
+        }
+
+        private void LBL10_Click(object sender, EventArgs e)
+        {
+            if (LBL0.Text == "0")
+            {
+                LBL0.Text = LBL10.Text;
+                LBL10.Text = "0";
+            }
+
+            if (LBL11.Text == "0")
+            {
+                LBL11.Text = LBL10.Text;
+                LBL10.Text = "0";
+            }
+
+            if (LBL20.Text == "0")
+            {
+                LBL20.Text = LBL10.Text;
+                LBL10.Text = "0";
+            }
+        }
+
+        private void LBL0_Click(object sender, EventArgs e)
+        {
+            if (LBL10.Text == "0")
+            {
+                LBL10.Text = LBL0.Text;
+                LBL0.Text = "0";
+            }
+
+            if (LBL01.Text == "0")
+            {
+                LBL01.Text = LBL0.Text;
+                LBL0.Text = "0";
+            }
+        }
+
+        private void LBL02_Click(object sender, EventArgs e)
+        {
+            if (LBL12.Text == "0")
+            {
+                LBL12.Text = LBL02.Text;
+                LBL02.Text = "0";
+            }
+
+            if (LBL01.Text == "0")
+            {
+                LBL01.Text = LBL02.Text;
+                LBL02.Text = "0";
+            }
+        }
+
+        private void LBL01_Click(object sender, EventArgs e)
+        {
+            if (LBL11.Text == "0")
+            {
+                LBL11.Text = LBL01.Text;
+                LBL01.Text = "0";
+            }
+
+            if (LBL0.Text == "0")
+            {
+                LBL0.Text = LBL01.Text;
+                LBL01.Text = "0";
+            }
+
+            if (LBL02.Text == "0")
+            {
+                LBL02.Text = LBL01.Text;
+                LBL01.Text = "0";
+            }
+        }
+
+        private void BTNDesordenar_Click(object sender, EventArgs e)
+        {
+            TMReloj.Enabled = true;
+        }
+
+        private void TMReloj_Tick(object sender, EventArgs e)
+        {
+            if(contador < 10)
+            {
+                contador++;
+                LBLContador.Text = contador.ToString();
+            }
+            else
+            {
+                TMReloj.Enabled=false;
+                MessageBox.Show("Reloj apagado");
+                LBLContador.Text = "";
+                contador = 0;
+            }
+        }
+    }
+}
